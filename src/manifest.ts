e8adaf656520b9bd31fe8cdf43d778be7dbb7553
leo03164 <leo03164@gmail.com>
2025-06-13 16:57:55 +0800

    Update icon map

--- src/manifest.ts
+++ src/manifest.ts
@@ -25,9 +25,9 @@ export async function getManifest() {
           service_worker: './dist/background/index.mjs',
         },
     icons: {
-      16: './assets/icon-512.png',
-      48: './assets/icon-512.png',
-      128: './assets/icon-512.png',
+      16: './assets/icon-16.png',
+      48: './assets/icon-48.png',
+      128: './assets/icon-128.png',
     },
     permissions: [
       'tabs',
