bcf97448de1a291fa22650efc472d1dd12d30d52
leo03164 <leo03164@gmail.com>
2025-06-13 17:28:33 +0800

    Update imge path

--- src/manifest.ts
+++ src/manifest.ts
@@ -14,7 +14,7 @@ export async function getManifest() {
     version: pkg.version,
     description: pkg.description,
     action: {
-      default_icon: './assets/icon-512.png',
+      default_icon: 'assets/icon-512.png',
     },
     background: isFirefox
       ? {
@@ -25,9 +25,9 @@ export async function getManifest() {
           service_worker: './dist/background/index.mjs',
         },
     icons: {
-      16: './assets/icon-16.png',
-      48: './assets/icon-48.png',
-      128: './assets/icon-128.png',
+      16: 'assets/icon-512.png',
+      48: 'assets/icon-512.png',
+      128: 'assets/icon-512.png',
     },
     permissions: [
       'tabs',
